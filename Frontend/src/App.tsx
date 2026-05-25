1eee1d8d639bc27c5435a9b7bbe5e8bba199fec5
Andrii Pervashov <pervashovandrii@gmail.com>
2026-05-25 09:48:41 +0200

    feat(frontend): improve app shell layout and toast configuration

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,29 @@
-import { Outlet } from "react-router";
-import "./App.css";
-import "react-toastify/dist/ReactToastify.css"
-import Navbar from "./components/Navbar/Navbar";
-import { ToastContainer } from "react-toastify";
-import { UserProvider } from "./Context/useAuth";
-import React from "react";
+import { Outlet } from 'react-router';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
+import { UserProvider } from './Context/useAuth';
 
 function App() {
   return (
-    <>
     <UserProvider>
-      <Navbar />
-      <Outlet />
-      <ToastContainer/>
-      </UserProvider>
-    </>
+      <div className="flex min-h-screen flex-col bg-surfaceCanvas">
+        <Navbar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <ToastContainer
+          position="top-right"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          theme="colored"
+          toastClassName="!rounded-btn !font-sans"
+        />
+      </div>
+    </UserProvider>
   );
 }
 

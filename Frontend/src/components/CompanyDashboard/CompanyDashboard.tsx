a34fcc8d5ee3a6f34498d01fb8095b7c90663293
Andrii Pervashov <pervashovandrii@gmail.com>
2026-05-25 09:48:49 +0200

    style(frontend): update company dashboard shell colors

--- Frontend/src/components/CompanyDashboard/CompanyDashboard.tsx
+++ Frontend/src/components/CompanyDashboard/CompanyDashboard.tsx
@@ -5,19 +5,20 @@ interface Props {
   children: React.ReactNode;
   ticker: string;
 }
+
 const CompanyDashboard = ({ children, ticker }: Props) => {
-    return (
-      <div className="relative md:ml-64 bg-blueGray-100 w-full">
-        <div className="relative pt-20 pb-32 bg-lightBlue-500">
-          <div className="px-4 md:px-6 mx-auto w-full">
-            <div>
-              <div className="flex flex-wrap">{children}</div>
-              <div className="flex flex-wrap">{<Outlet context={ticker} />}</div>
-            </div>
+  return (
+    <div className="relative w-full bg-surfaceMuted md:ml-64">
+      <div className="relative bg-brandLink pb-32 pt-20">
+        <div className="mx-auto w-full px-4 md:px-6">
+          <div className="flex flex-wrap">{children}</div>
+          <div className="flex flex-wrap">
+            <Outlet context={ticker} />
           </div>
         </div>
       </div>
-    ); 
-  };
+    </div>
+  );
+};
 
-export default CompanyDashboard
+export default CompanyDashboard;

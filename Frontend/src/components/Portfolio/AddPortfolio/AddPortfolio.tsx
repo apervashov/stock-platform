f30498f499275eedb5470596f31fe04a7ec833ef
Andrii Pervashov <pervashovandrii@gmail.com>
2026-05-25 09:48:48 +0200

    style(frontend): use secondary button on add portfolio action

--- Frontend/src/components/Portfolio/AddPortfolio/AddPortfolio.tsx
+++ Frontend/src/components/Portfolio/AddPortfolio/AddPortfolio.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent } from "react";
+import { SyntheticEvent } from 'react';
 
 interface Props {
   onPortfolioCreate: (e: SyntheticEvent) => void;
@@ -7,17 +7,15 @@ interface Props {
 
 const AddPortfolio = ({ symbol, onPortfolioCreate }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+    <div className="flex flex-1 flex-col items-center justify-end space-y-2 md:flex-row md:space-y-0">
       <form onSubmit={onPortfolioCreate}>
-        <input readOnly={true} hidden={true} value={symbol} />
-        <button
-          type="submit"
-          className="p-2 px-8 text-white bg-darkBlue rounded-lg hover:opacity-70 focus:outline-none"
-        >
+        <input readOnly hidden value={symbol} />
+        <button type="submit" className="btn-secondary">
           Add
         </button>
       </form>
     </div>
   );
 };
+
 export default AddPortfolio;
